f2786aec5d7cbe6a722abaa4e9a5122774fb8e7d
Ansh Bahl <anshbahl0@gmail.com>
2025-07-26 13:11:42 +0530

    Fix: Removed frontend submodule and added as a regular folder

---
+++ services/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Frontend of E-commerce App</h1>
+      <p>This is the React frontend for your microservices.</p>
+    </div>
+  );
+}
+
+export default App;
